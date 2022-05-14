4fac32a9e6313d64b7e8b9ec68d43dc0ee3afdff
e1essar <80064778+e1essar@users.noreply.github.com>
2022-05-15 03:47:21 +0700

    Add files via upload

---
+++ Huffman.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+// дерево
+struct Node {
+	char key;
+	int freq;
+	Node* left, * right;
+};
+
+// выделение узла дерева
+Node* GetNode(char key, int freq, Node* left, Node* right) {
+	Node* node = new Node();
+
+	node->key = key;
+	node->freq = freq;
+	node->left = left;
+	node->right = right;
+
+	return node;
+}
+
+// порядок
+struct compare {
+	bool operator()(Node* l, Node* r) {
+		return l->freq > r->freq;
+	}
+};
+
+
+int main()
+{
+
+}
+
+
