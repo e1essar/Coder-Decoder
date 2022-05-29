64a71e78e73e4d77892a41d98ac97d344bad65dd
e1essar <80064778+e1essar@users.noreply.github.com>
2022-05-29 20:53:23 +0700

    Add files via upload

---
+++ HuffmanCoding.cpp
@@ -0,0 +1,205 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <queue>
+#include <unordered_map>
+#include <map>
+
+using namespace std;
+
+struct Node {
+    char key;
+    unsigned int freq;
+    Node* left;
+    Node* right;
+};
+
+Node* AllocNode(char key, unsigned int freq, Node* left, Node* right) {
+    Node* node = new Node();
+    node->key = key;
+    node->freq = freq;
+    node->left = left;
+    node->right = right;
+    return node;
+};
+
+struct compare {
+    bool operator()(Node* l, Node* r)
+    {
+        return l->freq > r->freq;
+    }
+};
+
+void Code(Node* root, string bincode, unordered_map<char, string>& HuffCode) {
+    if (root == NULL) return;
+
+    if (!root->left && !root->right) {
+        HuffCode[root->key] = bincode;
+    }
+
+    Code(root->left, bincode + "0", HuffCode);
+    Code(root->right, bincode + "1", HuffCode);
+}
+
+void HuffmanCoder(const char* input_text = "text.txt", const char* output_text = "encode.txt") {
+    FILE* input;
+    fopen_s(&input, input_text, "r");
+    if (!input) exit(1);
+    unsigned char key;
+    map<char, int> freq;
+    while (!feof(input)) {
+        key = fgetc(input);
+        if (!feof(input)) {
+            freq[key]++;
+        }
+    }
+
+    char count_letters = freq.size();
+
+    fclose(input);
+
+    FILE* output;
+    fopen_s(&output, output_text, "wb +");
+    fopen_s(&input, input_text, "rb");
+    if (!input || !output) exit(1);
+
+    fputc(count_letters, output);
+    for (auto pair : freq) {
+        fputc(pair.first, output);
+        fputc(pair.second, output);
+    }
+
+    priority_queue<Node*, vector<Node*>, compare> pq;
+
+    for (auto pair : freq) {
+        pq.push(AllocNode(pair.first, pair.second, nullptr, nullptr));
+    }
+
+    Node* temp;
+
+    while (pq.size() != 1)
+    {
+
+        Node* left = pq.top(); pq.pop();
+        Node* right = pq.top(); pq.pop();
+
+        if (left->freq == right->freq && left->key < right->key) {
+            temp = left;
+            left = right;
+            right = temp;
+        }
+
+        int sum = left->freq + right->freq;
+        pq.push(AllocNode('\0', sum, left, right));
+    }
+
+    Node* root = pq.top();
+
+    unordered_map<char, string> HuffCode;
+    Code(root, "", HuffCode);
+
+    cout << "Huffman Codes are :\n" << '\n';
+    for (auto pair : HuffCode) {
+        cout << pair.first << " " << pair.second << '\n';
+    }
+
+    char fkey, symbol = 0;
+    int countOFbits = 0;
+
+    while (!feof(input))
+    {
+        fkey = fgetc(input);
+        if (!feof(input))
+        {
+            if (countOFbits + HuffCode[fkey].length() <= 8)
+            {
+                for (int i = 0; i < HuffCode[fkey].length(); i++)
+                {
+                    symbol = (symbol << 1) | (HuffCode[fkey][i] - '0');
+                }
+                countOFbits += HuffCode[fkey].length();
+            }
+            else
+            {
+                if (HuffCode[fkey].length() - countOFbits + 8 <= 8)
+                {
+                    for (int j = 0; j < 8 - countOFbits; j++)
+                    {
+                        symbol = (symbol << 1) | (HuffCode[fkey][j] - '0');
+                    }
+                    fputc(symbol, output);
+                    symbol = 0;
+                    for (int j = 8 - countOFbits; j < HuffCode[fkey].length(); j++) // 1011110(1|00000011)
+                    {
+                        symbol = (symbol << 1) | (HuffCode[fkey][j] - '0');
+                    }
+                    countOFbits = HuffCode[fkey].length() - 8 + countOFbits;
+                }
+                else
+                {
+                    for (int j = 0; j < 8 - countOFbits; j++)
+                    {
+                        symbol = (symbol << 1) | (HuffCode[fkey][j] - '0');
+                    }
+
+                    fputc(symbol, output);
+                    int i = 8 - countOFbits;
+                    countOFbits = 0;
+                    symbol = 0;
+
+                    for (i; i < HuffCode[fkey].length(); i++)
+                    {
+                        symbol = (symbol << 1) | (HuffCode[fkey][i] - '0');
+                        countOFbits++;
+                        if (countOFbits == 8)
+                        {
+                            fputc(symbol, output);
+                            countOFbits = 0;
+                            symbol = 0;
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            symbol = symbol << (8 - countOFbits);
+            fputc(symbol, output);
+        }
+    }
+
+    fclose(input);
+    fclose(output);
+}
+
+void compressValue(const char* input_text = "text.txt", const char* output_text = "encode.txt")
+{
+    long long file_size = 0;
+    long long compress_size = 0;
+    //FILE* input; FILE* output;
+    //fopen_s(&input, input_text, "r");
+    //fopen_s(&output, output_text, "r");
+
+    struct stat sb {};
+    struct stat se {};
+    // Finding compression ratio
+    if (!stat(input_text, &sb)) {
+        file_size = sb.st_size;
+    }
+    else {
+        perror("stat");
+    }
+    if (!stat(output_text, &se)) {
+        compress_size = se.st_size;
+    }
+    else {
+        perror("stat");
+    }
+    cout << "\nCompress value is: " << (compress_size + 0.0) / file_size << "\n";
+    return;
+}
+
+int main() {
+    HuffmanCoder();
+    //compressValue();
+}
---
+++ HuffmanDecoding.cpp
@@ -0,0 +1,179 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <queue>
+#include <unordered_map>
+#include <map>
+
+
+using namespace std;
+
+struct Node {
+    char key;
+    unsigned int freq;
+    Node* left;
+    Node* right;
+};
+
+Node* AllocNode(char key, unsigned int freq, Node* left, Node* right) {
+    Node* node = new Node();
+    node->key = key;
+    node->freq = freq;
+    node->left = left;
+    node->right = right;
+    return node;
+};
+
+struct compare {
+    bool operator()(Node* l, Node* r) {
+        return l->freq > r->freq;
+    }
+};
+
+void decode(Node* root, int& index, string str)
+{
+    if (root == nullptr) {
+        return;
+    }
+
+    // found a leaf node
+    if (!root->left && !root->right)
+    {
+        cout << root->key;
+        return;
+    }
+
+    index++;
+
+    if (str[index] == '0')
+        decode(root->left, index, str);
+    else
+        decode(root->right, index, str);
+}
+
+void HuffmanDecoder(const char* input_text = "encode.txt", const char* output_text = "decoded.txt") {
+    FILE* input = fopen(input_text, "rb");
+    unsigned char count = 0;
+    unsigned char key;
+    int cart = 0;
+    if (input != NULL) {
+        count = fgetc(input);
+    }
+
+    map<char, int> freq;
+    for (int i = 0; i < count; i++) {
+        key = fgetc(input);
+        freq[key] = fgetc(input);
+        cart += freq[key];
+    }
+
+    cout << (int)count << '\n';
+    cout << "Huffman Codes are :\n" << '\n';
+    for (auto pair : freq) {
+        cout << pair.first << " " << pair.second << '\n';
+    }
+
+    priority_queue<Node*, vector<Node*>, compare> pq;
+
+    for (auto pair : freq) {
+        pq.push(AllocNode(pair.first, pair.second, nullptr, nullptr));
+    }
+
+    Node* temp;
+
+    while (pq.size() != 1)
+    {
+
+        Node* left = pq.top(); pq.pop();
+        Node* right = pq.top(); pq.pop();
+
+        if (left->freq == right->freq && left->key < right->key) {
+            temp = left;
+            left = right;
+            right = temp;
+        }
+
+        int sum = left->freq + right->freq;
+        pq.push(AllocNode('\0', sum, left, right));
+    }
+
+    Node* root = pq.top();
+
+    FILE* output;
+
+    fopen_s(&output, output_text, "wb +");
+    char symbol; char letter;
+    while (!feof(input))
+    {
+        symbol = fgetc(input);
+        if (!feof(input))
+        {
+            for (int i = 7; i >= 0 && cart != 0; i--)
+            {
+                if ((symbol >> i) & 1)
+                {
+                    if (root->right)
+                    {
+                        root = root->right;
+                    }
+                    else
+                    {
+                        letter = root->key;
+                        fputc(letter, output);
+                        cart--;
+                        root = pq.top()->right;
+                    }
+                }
+                else
+                {
+                    if (root->left)
+                    {
+                        root = root->left;
+                    }
+                    else
+                    {
+                        letter = root->key;
+                        fputc(letter, output);
+                        cart--;
+                        root = pq.top()->left;
+                    }
+                }
+            }
+        }
+    }
+    fclose(input);
+    fclose(output);
+}
+
+void AreEqual(const char* input_text = "text.txt", const char* output_text = "umom.txt")
+{
+    FILE* input; FILE* output;
+    fopen_s(&input, input_text, "r");
+    fopen_s(&output, output_text, "r");
+
+    char a = fgetc(input), b = fgetc(output);
+    bool flag = true;
+
+    while (!feof(input) && flag)
+    {
+        flag = false;
+        if ((a & b) == a)
+        {
+            a = fgetc(input);
+            b = fgetc(output);
+            flag = true;
+        }
+    }
+    if (flag) cout << "Texts are equal" << '\n';
+    else cout << "Texts are not equal" << '\n';
+
+    fclose(input);
+    fclose(output);
+}
+
+int main()
+{
+    HuffmanDecoder();
+    //AreEqual();
+}
